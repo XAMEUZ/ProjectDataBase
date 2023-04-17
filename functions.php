aae9e651e8a43baf2a66d67ea1726cf75940ab99
XAMEUZ <115125912+XAMEUZ@users.noreply.github.com>
2023-04-17 20:29:46 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,3 +14,20 @@ return $rows;
 }
 function tambah ($data) {
 global $conn;
+  
+  function tambah($data){
+  global $conn;
+
+  $id = $data["id"];
+  $nama = $data["nama"];
+  $email = $data["email"];
+  $password = $data["password"];
+
+  $query = "INSERT INTO namadatabase
+      VALUES ('', '$id', '$nama', '$email', '$password')
+      ";
+  mysqli_query($conn, $query);
+
+  return mysqli_affected_rows($conn);  
+  //Kalau gagal ada output -1 , kalau data berhasil di tambah ada output +1 ( return mysqli_affected_rows)
+}
